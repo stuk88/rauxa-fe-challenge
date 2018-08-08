8cc6e56499a0d491918de2a94c473c0e29d4e801
stuk <stuk88@gmail.com>
2018-08-08 05:09:16 +0300

    print user and followers

--- src/App.js
+++ src/App.js
@@ -8,34 +8,47 @@ class App extends Component {
 
     super(props);
     this.state = {
-        user: '',
-        gh_user_handle: null,
-        results: []
+        username: '',
+        user: null,
+        followers: null,
+        hasResults: false
     };
 
   }
 
   handleUserTyped = (e) => {
-    this.setState({user: e.target.value});
+    this.setState({username: e.target.value});
   }
 
   searchGithub = async () => {
     try {
-      var user_req = await fetch(`https://api.github.com/users/${this.state.user}`);
+      var user_req = await fetch(`https://api.github.com/users/${this.state.username}`);
       var user = await user_req.json();
-      var followers_req = await fetch(`https://api.github.com/users/${this.state.user}/followers`);
+      var followers_req = await fetch(`https://api.github.com/users/${this.state.username}/followers`);
       var followers = await followers_req.json();
-
     
       this.setState({
-        gh_user_handle: user,
-        results: {user, followers}
+        user,
+        followers,
+        hasResults: true
       });
     } catch(e) {
       console.log(e);
+      this.setState({
+        hasResults: false
+      })
     }
 
   } 
+
+  const Follower = (props) => {
+    return (
+            <a href={props.user.url} target="_blank">
+                {props.user.login} 
+                <img style={{width:'150px',height: '150px'}} src={props.user.avatar_url} />
+              </a>
+      )
+  }
 
   render() {
     return (
@@ -48,9 +61,19 @@ class App extends Component {
             <input type="text" value={this.state.user} onChange={this.handleUserTyped} /> 
             <input type="button" value="Send" onClick={this.searchGithub} /> 
           </form>
-          {this.state.results && 
+          {this.state.hasResults && 
             <div className="results">
-              {JSON.stringify(this.state.results)}
+                User: {this.state.user.login} Followers: {this.state.user.followers}<br/>
+              <a href={this.state.user.url} target="_blank">
+                <img style={{width:'150px',height: '150px'}} src={this.state.user.avatar_url} />
+              </a>
+              {this.state.followers.length > 0 &&
+                <div className="followers">
+                  {this.state.followers.map((follower) => {
+                      <Follower user={follower} />
+                  })}
+                </div>
+              }
             </div>
           }
         </div>
